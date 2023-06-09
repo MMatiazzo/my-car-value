67d2dd4764a304946b8e2eee369feda3e5e3f599
MMatiazzo <mathmatiazzo@gmail.com>
2023-06-09 11:30:06 -0300

    feat: create report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post('/')
+  @UseGuards(AuthGuard)
+  async createRepor(
+    @Body() { make, model, year, mileage, lng, lat, price }: CreateReportDto,
+  ) {
+    return await this.reportsService.create({
+      make,
+      model,
+      year,
+      mileage,
+      lng,
+      lat,
+      price,
+    });
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Report } from './report.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  async create({
+    make,
+    model,
+    year,
+    mileage,
+    lng,
+    lat,
+    price,
+  }: CreateReportDto) {
+    const reportCreated = this.repo.create({
+      make,
+      model,
+      year,
+      mileage,
+      lng,
+      lat,
+      price,
+    });
+
+    await this.repo.save(reportCreated);
+
+    return reportCreated;
+  }
+}
